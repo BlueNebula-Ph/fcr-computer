83b9faa22d2882e06160338122d57d1ecebbbfe3
VonAerich <von.aerich@gmail.com>
2018-01-07 15:31:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var express = require('express'),
   path = require('path'),
   app = express();
 
-//set the port
-app.set('port', $Port);
+var port = process.env.PORT || 8080;
 
 //tell express that we want to use the www folder
 //for our static assets
 app.use(express.static(__dirname));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function () {
-  console.log('The server is running on https://fcr-computer.herokuapp.com:' + app.get('port'));
+app.get("/", function(request, response) {
+	response.render("index");
+});
+
+app.listen(port, function(){
+	console.log("app running...");
 });
